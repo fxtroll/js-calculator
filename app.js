271dd6be163adaa73b63386cf32346b42b710047
David White-Goode <dwhitegoode@gmail.com>
2022-10-09 17:51:31 -0400

    functions completed

--- app.js
+++ app.js
@@ -5,17 +5,42 @@ let calculator = {
     this.memory.push(a + b)
     return a + b
   },
-  sub: function () { },
-  mulitply: function () { },
-  divide: function () { },
-  exponent: function () { },
-  sqrt: function () { },
-  log: function (a) { },
+  sub: function (a, b = this.memory.pop()) {
+    this.memory.push(a - b)
+    return a - b;
+  },
+  mulitply: function (a, b = this.memory.pop()) {
+    this.memory.push(a * b)
+    return a * b;
+  },
+  divide: function (a, b = this.memory.pop()) {
+    this.memory.push(a / b)
+    return a / b;
+  },
+  exponent: function (a, b = this.memory.pop()) {
+    this.memory.push(a ** b)
+    return a ** b
+  },
+  root: function (a, b = this.memory.pop()) {
+    this.memory.push(a ** (1 / b))
+    return a ** (1 / b)
+  },
+  log: function (a) {
+    return Math.log(a)
+  },
+
+  // results are stored in array
   memory: [],
 
   clear: function () {
     this.memory.splice(0, this.memory.length)
+  },
+
+  stringOfOperations: function (a) {
+    this.memory.push(eval(a))
+    return eval(a)
   }
+
 }
 
 console.log(calculator.add(1, 5))
